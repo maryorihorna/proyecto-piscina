286ed793adf4372ac2f1f4a17f4f047709ffe53b
Lamk-L <kunlancelot@gmail.com>
2024-07-10 00:48:15 -0500

    Lamk

--- src/main/java/com/proyecto/piscina/web/app/config/SecurityConfig.java
+++ src/main/java/com/proyecto/piscina/web/app/config/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.proyecto.piscina.web.app.config;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
