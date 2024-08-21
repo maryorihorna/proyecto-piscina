5284178cf095d97a46376aaf52fa0199df72ab8f
JairVC18 <jairvasquez874@gmail.com>
2024-08-21 07:16:51 -0500

    P07 Docker Finish

--- src/main/java/com/proyecto/piscina/web/app/controllers/DashboardController.java
+++ src/main/java/com/proyecto/piscina/web/app/controllers/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +17,7 @@ import com.proyecto.piscina.web.app.services.MatriculaService;
 import com.proyecto.piscina.web.app.services.PagoService;
 
 @Controller
+@RequestMapping("/inicio")
 public class DashboardController {
     
     private AlumnoService alumnoService;
